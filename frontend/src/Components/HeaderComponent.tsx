44d079133e3961d212d3b127a2092e6fe88fd8d6
tharun87 <bowenitharunteja@gmail.com>
2026-02-07 23:59:40 +0530

    restructured files

---
+++ frontend/src/Components/HeaderComponent.tsx
@@ -0,0 +1,11 @@
+
+const HeaderComponent = () => {
+  return (
+    <header className='text-center'>
+      <h2 className='text-blue-700 font-bold text-4xl'>My Tasks</h2>
+      <p className="mt-3 text-gray-600">Stay organized and productive</p>
+    </header>
+  )
+}
+
+export default HeaderComponent
---
+++ frontend/src/redux_files/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducer from "./todoSlice"
+
+
+export const store = configureStore({
+    reducer:{
+        todo: todoReducer,
+    }
+}) 
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

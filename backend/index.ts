610517d109ffd57adc04e46f8bb476bc603f8635
tharun87 <bowenitharunteja@gmail.com>
2026-02-14 23:26:02 +0530

    created backend

---
+++ backend/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.mongoURI as string);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(
+      "MongoDB connection error:",
+      error instanceof Error ? error.message : error,
+    );
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+app.get("/",(req,res)=>{
+    res.send("Beckedn API is running")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
